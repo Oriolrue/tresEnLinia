1a4b8a3304eb614e4482817d5eb62626eba1ac6d
daw1 <orue@elpuig.xeill.net>
2024-04-17 17:26:00 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+    }
+}
