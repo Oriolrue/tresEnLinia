38256e620f09507639492e4d7836b0cc5369754f
Urize5 <animatronics11@gmail.com>
2024-04-29 12:19:10 +0200

    Desarrollo de pruebas unitarias

--- src/JuegoTest.java
+++ src/JuegoTest.java
@@ -1,9 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JuegoTest {
 
     @org.junit.jupiter.api.Test
     void nuevaPartida() {
+        Juego j = new Juego();
+        Assertions.assertEquals(1, j.getTurno());
+        Assertions.assertEquals(' ', j.getTablero());
     }
 
     @org.junit.jupiter.api.Test
