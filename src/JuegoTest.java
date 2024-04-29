d4c27497b7789cd739bdfc8d14593b615613f5c5
Urize5 <animatronics11@gmail.com>
2024-04-29 11:30:59 +0200

    Creada clase test juego, con sus pruebas unitarias

---
+++ src/JuegoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class JuegoTest {
+
+    @org.junit.jupiter.api.Test
+    void nuevaPartida() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void jugar() {
+    }
+}
