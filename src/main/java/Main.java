e37b2074b246eb3dd903440f959130a9aceba920
msole26 <msole@elpuig.xeill.net>
2024-05-14 11:23:56 +0200

    pruebas para poder mostrar el tablero

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,36 @@
 public class Main{
+    static TUI TUI = new TUI();
+    static Juego Juego = new Juego();
+
     public static void main(String[] args) {
 
-        TUI TUI = new TUI();
-        Juego Juego = new Juego();
+        while (true) {
+            int opcion = TUI.mostrarMenu();
 
-        TUI.mostrarMenu();
+            switch (opcion) {
+                case 1:
+                    Main.nuevaPartida();
+                    break;
+                case 2:
+                    // Load a saved game
+                    break;
+                case 3:
+                    // Display configuration options
+                    break;
+                case 4:
+                    return; // Exit the program
+                default:
 
-
+            }
+        }
 
         // aqui tiene que haber metodo nuevapartida() con un switch, que pida una nueva partida a "Juego"
         // pedimos tablero a juego y a tui le pedimos que muestre el tablero
         // pedirle al jugador 1 la jugada a tui y que se lo devuelva a main y que llame a juego para que haga la jugada
         // tui y juego no se pueden comunicar directamente entre ellos
+    }
 
+    public static void nuevaPartida(){
+        TUI.mostrarTablero(Juego.getTablero(), Juego.getTurno());
     }
 }
